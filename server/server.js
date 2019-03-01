b53a3f27f461cc5625562f912f024d8ab15869ce
Karol Wyrzykowski <karol.wyrzykowski128@gmail.com>
2019-03-01 22:10:46 +0100

    change .idea in gitingore

--- server/server.js
+++ server/server.js
@@ -2,14 +2,18 @@ const express = require("express");
 const path = require('path')//to jest budowane nie trzeba instalowac
 const publicPath = path.join(__dirname,'../public') // dirname to folder w którym jest ten glowny plik
 const port = process.env.PORT || 3000;
+const socketIO = require('socket.io');
+const http = require('http') //wbudowana biblioteka
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+
 app.use(express.static(publicPath));
-app.get("/",(req,res)=>{
-    res.send("index.html")
-})
 
 
-app.listen(port,()=>{
+
+server.listen(port,()=>{
     console.log(`server is up on port ${port}`);
 });
